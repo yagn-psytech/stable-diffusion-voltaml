8c15a0767960391b4f251e7611fe07e8ded9efb4
Sahil Chaudhary <sahil280114@gmail.com>
2022-12-22 14:35:04 +0100

    Update download.py

--- download.py
+++ download.py
@@ -4,13 +4,13 @@
 from diffusers import StableDiffusionPipeline, LMSDiscreteScheduler
 import os
 from volta_accelerate import compile_trt
-def download_model():
+def build_model():
     # do a dry run of loading the huggingface model, which will download weights at build time
     #Set auth token which is required to download stable diffusion model weights
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    MODEL_NAME = "runwayml/stable-diffusion-v1-5"#os.getenv("MODEL_NAME")
+    MODEL_NAME = os.getenv("MODEL_NAME")
 
     compile_trt(MODEL_NAME,HF_AUTH_TOKEN)
 
 if __name__ == "__main__":
-    download_model()
+    build_model()
